4b43b5b9a3b5e176c43393eae0b77ddf698e8655
simart <simart@gmail.com>
2014-06-10 14:47:05 -0700

    fiddled with opacity

--- build-tmp/source/ImageHowTo.java
+++ build-tmp/source/ImageHowTo.java
@@ -17,19 +17,18 @@ public class ImageHowTo extends PApplet {
 PImage b;
 public void setup()
 {
-	size(450,350);
+	size(450,310);
 	b = loadImage("http://i.imgur.com/l2TxLo8.png");
 	frameRate(1);
 	background(0);
-	fill(255);
-	textAlign(CENTER);
-	textSize(24);
-	text("This picture is in the program",225,320);
+
 }
 public void draw()
 {
-	tint(255,255,255,200);
-	image(b, random(width-284), random(height-219),284,169);
+	stroke(0,0,0,80);
+	fill(0,0,0,80);
+	rect(0,0,width,height);
+	image(b, random(width-284), random(height-169),284,169);
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "ImageHowTo" };
